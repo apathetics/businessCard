4f52f1410e23e53e4c24922a8c3e384d828ecc40
Tracy Nguyen <apathetictracyn@gmail.com>
2019-02-06 21:41:09 -0600

    Initial interface skeleton and Travis CI set-up.
    - Set up classes, project structure, and basic test hook into Travis.

---
+++ src/main/java/com/tracy/businessCard/BusinessCardParser.java
@@ -0,0 +1,4 @@
+package com.tracy.businessCard;
+
+public class BusinessCardParser {
+}
---
+++ src/test/java/com/tracy/businessCard/ContactInfoTest.java
@@ -0,0 +1,12 @@
+package com.tracy.businessCard;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class ContactInfoTest {
+    @Test
+    public void testContactInfo() throws Exception {
+        Assert.assertTrue(1 == 1);
+    }
+
+}

2a8c15ed0d6e54e6123214f82ca41cedb70df07c
Tracy Nguyen <apathetictracyn@gmail.com>
2019-02-07 00:20:43 -0600

    Email detection with regex implemented.

--- src/main/java/com/tracy/businessCard/BusinessCardParser.java
+++ src/main/java/com/tracy/businessCard/BusinessCardParser.java
@@ -1,5 +1,8 @@
 package com.tracy.businessCard;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class BusinessCardParser {
 
     public static ContactInfo getContactInfo(String document) {
@@ -13,20 +16,46 @@ public class BusinessCardParser {
         return parsedContactInfo;
     }
 
+    // Personally, I would make these auxiliary methods private and just write a single test for getContactInfo().
+    // For the sake of demonstrating fine-grained testing though, I've made them public.
+
     // Named Entity Problem - NLP
     // brute-force: use dictionary look-up for most common names?
-    private static String parseName(String document) {
+    public static String parseName(String document) {
         return "a";
     }
 
     // use regex to find 3 + 3 + 4 or 1 + 3 + 3 + 4 numbers
     // Can be confused with fax - if "fax" in line, then skip.
-    private static String parseNumber(String document) {
+    public static String parseNumber(String document) {
         return "b";
     }
 
-    // use regex to find @ in the line
-    private static String parseEmail(String document) {
-        return "c";
+    // use regex to find @ in the line and validate email
+    public static String parseEmail(String document) {
+
+        // Assumes business card input is correct without any invalid email formats.
+        String simpleRegex = "^(.+)@(.+)$";
+
+        // Catches double dots, leading dots, and trailing dots.
+        // Standard OWASP regex for emails.
+        String strictRegex = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-z A-Z]{2,7}$";
+
+        Pattern pattern = Pattern.compile(strictRegex);
+
+        // Covers Windows and Mac/Linux new lines.
+        String documentLines[] = document.split("\\r?\\n");
+
+        for(String email: documentLines) {
+            Matcher matcher = pattern.matcher(email);
+
+            // successful match found - exit
+            if(matcher.matches()) {
+                return email;
+            }
+        }
+
+        // Error Case - email not found.
+        return "EMAIL_NOT_FOUND";
     }
 }
--- src/test/java/com/tracy/businessCard/BusinessCardParserTest.java
+++ src/test/java/com/tracy/businessCard/BusinessCardParserTest.java
@@ -1,39 +1,37 @@
 package com.tracy.businessCard;
 
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 public class BusinessCardParserTest {
 
-    @Before
-    public void setUp() throws Exception {
-        String testCase1 = "ASYMMETRIK LTD\n" +
-                "Mike Smith\n" +
-                "Senior Software Engineer\n" +
-                "(410)555-1234\n" +
-                "msmith@asymmetrik.com";
+    String testCase1 = "ASYMMETRIK LTD\n" +
+            "Mike Smith\n" +
+            "Senior Software Engineer\n" +
+            "(410)555-1234\n" +
+            "msmith@asymmetrik.com";
 
-        String testCase2 = "Foobar Technologies\n" +
-                "Analytic Developer\n" +
-                "Lisa Haung\n" +
-                "1234 Sentry Road\n" +
-                "Columbia, MD 12345\n" +
-                "Phone: 410-555-1234\n" +
-                "Fax: 410-555-4321\n" +
-                "lisa.haung@foobartech.com";
+    String testCase2 = "Foobar Technologies\n" +
+            "Analytic Developer\n" +
+            "Lisa Haung\n" +
+            "1234 Sentry Road\n" +
+            "Columbia, MD 12345\n" +
+            "Phone: 410-555-1234\n" +
+            "Fax: 410-555-4321\n" +
+            "lisa.haung@foobartech.com";
 
-        String testCase3 = "Arthur Wilson\n" +
-                "Software Engineer\n" +
-                "Decision & Security Technologies\n" +
-                "ABC Technologies\n" +
-                "123 North 11th Street\n" +
-                "Suite 229\n" +
-                "Arlington, VA 22209\n" +
-                "Tel: +1 (703) 555-1259\n" +
-                "Fax: +1 (703) 555-1200\n" +
-                "awilson@abctech.com";
-    }
+    String testCase3 = "Arthur Wilson\n" +
+            "Software Engineer\n" +
+            "Decision & Security Technologies\n" +
+            "ABC Technologies\n" +
+            "123 North 11th Street\n" +
+            "Suite 229\n" +
+            "Arlington, VA 22209\n" +
+            "Tel: +1 (703) 555-1259\n" +
+            "Fax: +1 (703) 555-1200\n" +
+            "awilson@abctech.com";
 
     @Test
     public void testParseName() throws Exception {
@@ -47,6 +45,14 @@ public class BusinessCardParserTest {
 
     @Test
     public void testParseEmail() throws Exception {
-        Assert.assertTrue(1 == 1);
+
+        String badEmails = "bad@email..com\n" +
+                ".bad@email.com\n" +
+                "bad@email.com.\n";
+
+        Assert.assertEquals("msmith@asymmetrik.com", BusinessCardParser.parseEmail(testCase1));
+        Assert.assertEquals("lisa.haung@foobartech.com", BusinessCardParser.parseEmail(testCase2));
+        Assert.assertEquals("awilson@abctech.com", BusinessCardParser.parseEmail(testCase3));
+        Assert.assertEquals("EMAIL_NOT_FOUND", BusinessCardParser.parseEmail(badEmails));
     }
 }
